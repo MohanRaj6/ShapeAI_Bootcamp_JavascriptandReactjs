25a63d33d7c1e856651db3ee6c586a45d77c4ae1
MohanRaj6 <86546794+MohanRaj6@users.noreply.github.com>
2021-06-26 23:18:23 -0700

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return (
+    <div className="note">
+     <h1>Javascript and React.js</h1>
+     <p>This was an amazing bootcamp, I've learned a lot .Thanks to shapeAI and Shaurya Sinha.</p>
+    </div> 
+  );
+}
+
+export default Info;
